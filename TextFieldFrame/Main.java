e42436920e432da699513b665efc2e31c1c71496
Leuz1nn <158386732+Leuz1nn@users.noreply.github.com>
2024-02-28 14:25:40 -0400

    Add files via upload

---
+++ TextFieldFrame/Main.java
@@ -0,0 +1,12 @@
+package TextFieldFrame;
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        Textfieldframe textfieldFrame = new Textfieldframe();
+        textfieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textfieldFrame.setSize(350, 100);
+        textfieldFrame.setVisible(true);
+
+    }
+    
+}
