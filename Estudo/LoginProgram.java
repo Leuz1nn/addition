5e17fff1e1da2ad8a2a3d8187e65b29e41a0d869
Leuz1nn <158386732+Leuz1nn@users.noreply.github.com>
2024-03-13 14:46:19 -0400

    Add files via upload

---
+++ Estudo/LoginProgram.java
@@ -0,0 +1,56 @@
+package Estudo;
+import javax.swing.*;
+
+public class LoginProgram extends JFrame {
+    private JLabel labelUsername, labelPassword, labelImage;
+    private JTextField textFieldUsername;
+    private JPasswordField passwordField;
+    private JButton buttonOK;
+
+    public LoginProgram() {
+        setTitle("Login Program");
+        setSize(550, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+
+        // Adicionando imagem
+        ImageIcon imageIcon = new ImageIcon(getClass().getResource("ifmt.png"));
+        labelImage = new JLabel(imageIcon);
+        labelImage.setBounds(10, 10, 150, 150);
+        add(labelImage);
+
+        labelUsername = new JLabel("Username:");
+        labelUsername.setBounds(180, 20, 80, 25);
+        add(labelUsername);
+
+        textFieldUsername = new JTextField();
+        textFieldUsername.setBounds(260, 20, 150, 25);
+        add(textFieldUsername);
+
+        labelPassword = new JLabel("Password:");
+        labelPassword.setBounds(180, 50, 80, 25);
+        add(labelPassword);
+
+        passwordField = new JPasswordField();
+        passwordField.setBounds(260, 50, 150, 25);
+        add(passwordField);
+
+        buttonOK = new JButton("OK");
+        buttonOK.setBounds(260, 80, 80, 25);
+        buttonOK.addActionListener(e -> {
+            String username = textFieldUsername.getText();
+            String password = new String(passwordField.getPassword());
+
+            JOptionPane.showMessageDialog(LoginProgram.this,
+                    "Username: " + username + "\nPassword: " + password,
+                    "Login Information",
+                    JOptionPane.INFORMATION_MESSAGE);
+        });
+        add(buttonOK);
+
+        setVisible(true);
+    }}
+
+
+
+    
